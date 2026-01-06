9077852d93bc3a633a5895e5459f731d72922b3d
Camio1945 <camio1945@qq.com>
2026-01-06 10:09:04 +0800

    set created_by and updated by

--- src/main/java/cn/huava/common/pojo/po/BasePo.java
+++ src/main/java/cn/huava/common/pojo/po/BasePo.java
@@ -1,5 +1,9 @@
 package cn.huava.common.pojo.po;
 
+import static cn.huava.common.constant.CommonConstant.ADMIN_USER_ID;
+
+import cn.huava.common.util.Fn;
+import cn.huava.sys.pojo.po.UserPo;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.io.Serial;
@@ -7,12 +11,14 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 
 ///
 /// # Base PO class
 ///
 /// @author Camio1945
+@Slf4j
 @Data
 public class BasePo implements Serializable {
   @Serial private static final long serialVersionUID = 1L;
@@ -40,14 +46,29 @@ public class BasePo implements Serializable {
     if (entity instanceof BasePo basePo) {
       Date date = new Date();
       basePo.setId(null);
+      Long userId = getLoginUserId();
+      basePo.setCreatedBy(userId);
       basePo.setCreatedAt(date);
+      basePo.setUpdatedBy(userId);
       basePo.setUpdatedAt(date);
       basePo.setDeleteInfo(0L);
     }
   }
 
+  private static Long getLoginUserId() {
+    UserPo loginUser;
+    try {
+      loginUser = Fn.getLoginUser();
+      return loginUser.getId();
+    } catch (Exception e) {
+      log.warn("Failed to get login user: {}", e.getMessage());
+      return ADMIN_USER_ID;
+    }
+  }
+
   public static <T> void beforeUpdate(@NonNull T entity) {
     if (entity instanceof BasePo basePo) {
+      basePo.setUpdatedBy(getLoginUserId());
       basePo.setUpdatedAt(new Date());
     }
   }
@@ -57,6 +78,8 @@ public class BasePo implements Serializable {
       Date now = new Date();
       SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
       String timeStr = sdf.format(now);
+      basePo.setUpdatedBy(getLoginUserId());
+      basePo.setUpdatedAt(now);
       basePo.setDeleteInfo(Long.parseLong(timeStr));
     }
   }
