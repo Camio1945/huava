5f7e6ab04007336f3d81bbc0b8df36697a0f9b20
Camio1945 <camio1945@qq.com>
2026-01-16 18:39:14 +0800

    feat(common): add nullability annotations and suppress warnings in UploadToLocalServiceImpl

--- src/main/java/cn/huava/common/service/attachment/UploadToLocalServiceImpl.java
+++ src/main/java/cn/huava/common/service/attachment/UploadToLocalServiceImpl.java
@@ -16,6 +16,8 @@ import java.nio.file.Paths;
 import java.util.Date;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,18 +27,25 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
  * 将附件上传到本地磁盘的文件夹中，文件夹的路径在 application.yml 中定义，见 attachment 相关配置<br>
  * 这个类没有被显式地调用，而是在反射中用到。
  *
+ * <pre>
+ * 关于 SuppressWarnings :
+ *   java:S110： Inheritance tree of classes should not be too deep，我们的业务代码其实不深，但是框架内部本身占了三层，导致总层数超过 5 层，这里直接忽略。
+ * </pre>
+ *
  * @author Camio1945
  */
 @Slf4j
 @Service
+@NullMarked
 @RequiredArgsConstructor
+@SuppressWarnings("java:S110")
 public class UploadToLocalServiceImpl extends BaseUploadService {
 
   @Value("${project.attachment.path}")
   private String attachmentPath;
 
   @Override
-  protected AttachmentPo upload(@NonNull MultipartHttpServletRequest req) {
+  protected AttachmentPo upload(MultipartHttpServletRequest req) {
     MultipartFile file = getMultipartFile(req);
     AttachmentPo attachmentPo = buildAttachmentPo(file);
     String url = saveFile(file);
@@ -46,7 +55,7 @@ public class UploadToLocalServiceImpl extends BaseUploadService {
   }
 
   @SuppressWarnings("java:S2637") // SonarLint 检测不到 Assert 的语义，实际上代码没有问题，不会返回 null，所以这里直接忽略
-  private static @NonNull MultipartFile getMultipartFile(final MultipartHttpServletRequest req) {
+  private static MultipartFile getMultipartFile(final MultipartHttpServletRequest req) {
     MultipartFile file = req.getFile(MULTIPART_PARAM_NAME);
     Assert.isTrue(file != null && !file.isEmpty(), "解析不到上传的文件");
     return file;
@@ -70,7 +79,7 @@ public class UploadToLocalServiceImpl extends BaseUploadService {
     return destFilePath.replace(attachmentPath, "");
   }
 
-  private String buildFilePath(String fileName) {
+  private String buildFilePath(@Nullable String fileName) {
     if (!Paths.get(attachmentPath).isAbsolute()) {
       attachmentPath = System.getProperty("user.home") + File.separator + attachmentPath;
     }
@@ -83,7 +92,7 @@ public class UploadToLocalServiceImpl extends BaseUploadService {
     return Fn.cleanPath(folderPath + File.separator + IdUtil.fastSimpleUUID() + ext);
   }
 
-  private static String getExt(String fileName) {
+  private static String getExt(@Nullable String fileName) {
     String ext = FileNameUtil.extName(fileName);
     ext = Fn.isBlank(ext) ? "" : "." + ext;
     ext = ext.toLowerCase();
