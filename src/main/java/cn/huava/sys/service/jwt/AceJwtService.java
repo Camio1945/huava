181503b2f0f4ef9fa2c00563a469eaf097c17a37
Camio1945 <camio1945@qq.com>
2026-01-16 17:28:04 +0800

    AceJwtServiceTest

--- src/main/java/cn/huava/sys/service/jwt/AceJwtService.java
+++ src/main/java/cn/huava/sys/service/jwt/AceJwtService.java
@@ -2,13 +2,12 @@ package cn.huava.sys.service.jwt;
 
 import cn.huava.common.constant.CommonConstant;
 import cn.huava.sys.pojo.dto.UserJwtDto;
+import cn.hutool.v7.core.lang.Assert;
+import cn.hutool.v7.json.jwt.JWT;
 import java.util.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NullMarked;
-import cn.hutool.v7.core.lang.Assert;
-import cn.hutool.v7.json.jwt.JWT;
-import cn.hutool.v7.json.jwt.JWTUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +23,8 @@ import org.springframework.stereotype.Service;
 public class AceJwtService {
   private final CreateTokenService createTokenService;
 
+  private final JwtUtilService jwtUtilService = new JwtUtilService();
+
   @Value("${project.jwt_key_base64}")
   private String jwtKeyBase64;
 
@@ -31,18 +32,18 @@ public class AceJwtService {
     return createTokenService.createToken(userId, getJwtKeyBytes());
   }
 
+  private byte[] getJwtKeyBytes() {
+    return Base64.getDecoder().decode(jwtKeyBase64);
+  }
+
   public Long getUserIdFromAccessToken(final String token) {
-    return JWTUtil.parseToken(token).getPayload("sub", Long.class);
+    return jwtUtilService.parseToken(token).getPayload("sub", Long.class);
   }
 
   public boolean isTokenExpired(final String token) {
-    Assert.isTrue(JWTUtil.verify(token, getJwtKeyBytes()), "invalid token");
-    JWT jwt = JWTUtil.parseToken(token);
+    Assert.isTrue(jwtUtilService.verify(token, getJwtKeyBytes()), "invalid token");
+    JWT jwt = jwtUtilService.parseToken(token);
     Long exp = jwt.getPayload("exp", Long.class);
     return exp != null && exp * CommonConstant.MILLIS_PER_SECOND <= System.currentTimeMillis();
-  }
-
-  private byte[] getJwtKeyBytes() {
-    return Base64.getDecoder().decode(jwtKeyBase64);
   }
 }
