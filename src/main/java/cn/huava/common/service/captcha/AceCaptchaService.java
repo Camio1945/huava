d5a320b8380aba380573ce6bab5a1167153fcf37
Camio1945 <camio1945@qq.com>
2026-01-16 15:50:35 +0800

    Remove RefreshCaptchaService.java

--- src/main/java/cn/huava/common/service/captcha/AceCaptchaService.java
+++ src/main/java/cn/huava/common/service/captcha/AceCaptchaService.java
@@ -1,8 +1,6 @@
 package cn.huava.common.service.captcha;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,12 +19,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AceCaptchaService {
-  private final RefreshCaptchaService refreshCaptchaService;
   private final ValidateCaptchaService validateCaptchaService;
-
-  public void refresh(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    refreshCaptchaService.refresh(req, resp);
-  }
 
   public void validate(
       HttpServletRequest req, String captchaCode, Boolean isCaptchaDisabledForTesting) {
